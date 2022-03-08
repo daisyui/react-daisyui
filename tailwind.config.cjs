bd6198ea39a60b33f1483f1c89764ec360c014b7
Benji <64439681+benjitrosch@users.noreply.github.com>
2022-03-08 11:18:24 -0500

    fix: stopped tailwind 3 jit from not creating styles for unused daisyui classes (#4)

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,19 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  safelist: [
+    {
+      pattern: /./
+    },
+  ],
   daisyui: {
     styled: true,
     themes: true,
     base: true,
     utils: true,
     logs: true,
+    rtl: false,
+    prefix: "",
+    darkTheme: "dark",
   },
   plugins: [require("daisyui")],
 }
