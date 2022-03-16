b43200ec44b6c299e3bc9dcbefb5dc9e12e02bb7
Christian Blandford <christianblandford@me.com>
2022-03-15 17:21:38 -0700

    feat: persistent storybook global theme

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,13 +1,16 @@
+import { addons } from '@storybook/addons'
+import { themes } from '@storybook/theming'
 import React from 'react'
 
 import StoryLayout from './story-layout'
 
 import '../src/styles.css'
+import { DEFAUlT_THEME, STORAGE_KEY, THEME_PICKER_LIST } from './theming'
 
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-  "previewTabs": {
-    'storybook/docs/panel': { hidden: true }
+  actions: { argTypesRegex: '^on[A-Z].*' },
+  previewTabs: {
+    'storybook/docs/panel': { hidden: true },
   },
   controls: {
     matchers: {
@@ -19,25 +22,37 @@ export const parameters = {
   options: {
     storySort: {
       order: [
-        "Home",
-        "Actions",
-        "Data Display",
-        "Data Input",
-        "Layout",
-        "Navigation",
-        "Mockup",
-        "Utils"
-      ]
-    }
+        'Home',
+        'Actions',
+        'Data Display',
+        'Data Input',
+        'Layout',
+        'Navigation',
+        'Mockup',
+        'Utils',
+      ],
+    },
+  },
+  themes: {
+    default: window.localStorage.getItem(STORAGE_KEY) || DEFAUlT_THEME,
+    onChange: (theme) => {
+      // STORAGE_KEY does not work in onChange... not sure why
+      if (theme) {
+        window.localStorage.setItem(
+          'sb-react-daisyui-preview-theme',
+          theme.class
+        )
+      } else {
+        window.localStorage.removeItem('sb-react-daisyui-preview-theme')
+      }
+    },
+    list: THEME_PICKER_LIST,
   },
 }
 
 export const decorators = [
   (Story, options) => (
-    <StoryLayout
-      title={options.title}
-      description={options.story}
-    >
+    <StoryLayout title={options.title} description={options.story}>
       <Story />
     </StoryLayout>
   ),
--- .storybook/story-layout.tsx
+++ .storybook/story-layout.tsx
@@ -1,9 +1,11 @@
 import React, { ReactNode, useState } from 'react'
 
 import { DEFAULT_THEMES } from '../src/defaultThemes'
+import { useGlobalTheme } from './theming'
 
 import Navbar from '../src/Navbar'
 import Select from '../src/Select'
+import Theme from '../src/Theme'
 
 type Props = {
   children: ReactNode | ReactNode[]
@@ -12,25 +14,15 @@ type Props = {
 }
 
 const StoryLayout = ({ children, title, description }: Props): JSX.Element => {
-  const [dataTheme, setDataTheme] = useState<string>('dark')
+  const globalTheme = useGlobalTheme()
+  console.log({ globalTheme })
 
   return (
-    <div data-theme={dataTheme} className="w-full h-screen p-8 bg-base-100">
+    <Theme dataTheme={globalTheme} className="w-full h-screen p-8 bg-base-100">
       <Navbar className="p-0 border-b border-neutral text-base-content">
         <Navbar.Start>
-          <span className="text-lg font-bold">
-            react-daisyui
-          </span>
+          <span className="text-lg font-bold">react-daisyui</span>
         </Navbar.Start>
-
-        <Navbar.End className="flex gap-x-4">
-          theme:
-          <Select value={dataTheme} onChange={(theme) => setDataTheme(theme)}>
-            {DEFAULT_THEMES.map((theme) => {
-              return <Select.Option value={theme}>{theme}</Select.Option>
-            })}
-          </Select>
-        </Navbar.End>
       </Navbar>
 
       <div className="w-full h-full my-4">
@@ -38,7 +30,7 @@ const StoryLayout = ({ children, title, description }: Props): JSX.Element => {
         <p className="text-base-content">{description}</p>
         <div className="my-4">{children}</div>
       </div>
-    </div>
+    </Theme>
   )
 }
 
