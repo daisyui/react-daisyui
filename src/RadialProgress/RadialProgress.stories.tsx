39b8f423047229d85d5826565ff730f56b1cb439
Benji Trosch <benjitrosch@gmail.com>
2022-04-15 13:29:13 -0400

    chore(Radial Progress): added size and thickness props and stories

--- src/RadialProgress/RadialProgress.stories.tsx
+++ src/RadialProgress/RadialProgress.stories.tsx
@@ -8,10 +8,33 @@ export default {
   component: RadialProgress,
 } as Meta
 
-export const Default: Story<RadialProgressProps> = (args) => {
+const Template: Story<RadialProgressProps> = (args) => {
   return <RadialProgress {...args}>{args.value}%</RadialProgress>
 }
 
+export const Default = Template.bind({})
 Default.args = {
   value: 75,
 }
+
+export const CustomColor = Template.bind({})
+CustomColor.args = {
+  value: 75,
+  color: 'primary'
+}
+
+export const BackgroundColor = Template.bind({})
+BackgroundColor.args = {
+  value: 75,
+  className: 'bg-primary text-primary-content border-4 border-primary'
+}
+
+export const CustomSizeAndThickness: Story<RadialProgressProps> = (args) => {
+  return (
+    <div className='flex items-center gap-4'>
+      <RadialProgress value={70} size='12rem' thickness='2px'>70%</RadialProgress>
+      <RadialProgress value={80} size='12rem' thickness='2rem'>80%</RadialProgress>
+    </div>
+  )
+}
+CustomSizeAndThickness.args = {}
