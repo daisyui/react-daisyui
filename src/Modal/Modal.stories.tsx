d8e056ae1bab49553b4d67053b0a986920e059ad
yoshi6jp <yoshi6jp@gmail.com>
2022-06-22 00:56:12 +0900

    docs(Modal): add storybook of modal (#121)

--- src/Modal/Modal.stories.tsx
+++ src/Modal/Modal.stories.tsx
@@ -17,25 +17,110 @@ export const Default: Story<ModalProps> = (args) => {
   }
 
   return (
-    <>
+    <div className="font-sans">
       <Button onClick={toggleVisible}>Open Modal</Button>
-      <Modal {...args} open={visible} onClickBackdrop={toggleVisible}>
-        <Modal.Header>Lorem Ipsum</Modal.Header>
+      <Modal {...args} open={visible}>
+        <Modal.Header className="font-bold">
+          Congratulations random Interner user!
+        </Modal.Header>
 
         <Modal.Body>
-          Enim dolorem dolorum omnis atque necessitatibus. Consequatur aut
-          adipisci qui iusto illo eaque. Consequatur repudiandae et. Nulla ea
-          quasi eligendi. Saepe velit autem minima.
+          You've been selected for a chance to get one year of subscription to
+          use Wikipedia for free!
         </Modal.Body>
 
         <Modal.Actions>
-          <Button onClick={toggleVisible} color="primary">
-            Accept
-          </Button>
-          <Button onClick={toggleVisible}>Cancel</Button>
+          <Button onClick={toggleVisible}>Yay!</Button>
         </Modal.Actions>
       </Modal>
-    </>
+    </div>
   )
 }
-Default.args = {}
+
+export const CloseButton: Story<ModalProps> = (args) => {
+  const [visible, setVisible] = useState<boolean>(false)
+
+  const toggleVisible = () => {
+    setVisible(!visible)
+  }
+
+  return (
+    <div className="font-sans">
+      <Button onClick={toggleVisible}>Open Modal</Button>
+      <Modal {...args} open={visible}>
+        <Button
+          size="sm"
+          shape="circle"
+          className="absolute right-2 top-2"
+          onClick={toggleVisible}
+        >
+          ✕
+        </Button>
+        <Modal.Header className="font-bold">
+          Congratulations random Interner user!
+        </Modal.Header>
+
+        <Modal.Body>
+          You've been selected for a chance to get one year of subscription to
+          use Wikipedia for free!
+        </Modal.Body>
+      </Modal>
+    </div>
+  )
+}
+
+export const ClickedOutside: Story<ModalProps> = (args) => {
+  const [visible, setVisible] = useState<boolean>(false)
+
+  const toggleVisible = () => {
+    setVisible(!visible)
+  }
+
+  return (
+    <div className="font-sans">
+      <Button onClick={toggleVisible}>Open Modal</Button>
+      <Modal {...args} open={visible} onClickBackdrop={toggleVisible}>
+        <Modal.Header className="font-bold">
+          Congratulations random Interner user!
+        </Modal.Header>
+
+        <Modal.Body>
+          You've been selected for a chance to get one year of subscription to
+          use Wikipedia for free!
+        </Modal.Body>
+      </Modal>
+    </div>
+  )
+}
+
+export const CustomWidth: Story<ModalProps> = (args) => {
+  const [visible, setVisible] = useState<boolean>(false)
+
+  const toggleVisible = () => {
+    setVisible(!visible)
+  }
+
+  return (
+    <div className="font-sans">
+      <Button onClick={toggleVisible}>Open Modal</Button>
+      <Modal {...args} open={visible}>
+        <Modal.Header className="font-bold">
+          Congratulations random Interner user!
+        </Modal.Header>
+
+        <Modal.Body>
+          You've been selected for a chance to get one year of subscription to
+          use Wikipedia for free!
+        </Modal.Body>
+
+        <Modal.Actions>
+          <Button onClick={toggleVisible}>Yay!</Button>
+        </Modal.Actions>
+      </Modal>
+    </div>
+  )
+}
+
+CustomWidth.args = {
+  className: 'w-11/12 max-w-5xl',
+}
