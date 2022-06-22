ac00a9f124ee1d30f5e1e39340aea8d906f419ad
redxzeta <xzetanzero@gmail.com>
2022-06-21 23:13:02 -0700

    docs: add storybook of hero

--- src/Hero/Hero.stories.tsx
+++ src/Hero/Hero.stories.tsx
@@ -2,10 +2,18 @@ import React from 'react'
 import { Story, Meta } from '@storybook/react'
 
 import Hero, { HeroProps } from '.'
+import Button from '../Button'
+import Card from '../Card'
+import Input from '../Input'
+import Form from '../Form'
+import Link from '../Link'
 
 export default {
   title: 'Layout/Hero',
   component: Hero,
+  args: {
+    className: 'bg-base-200 ',
+  },
 } as Meta
 
 export const Default: Story<HeroProps> = (args) => {
@@ -20,11 +28,105 @@ export const Default: Story<HeroProps> = (args) => {
             excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
             a id nisi.
           </p>
-          <button className="btn btn-primary">Get Started</button>
+
+          <Button color="primary">Get Started</Button>
         </div>
       </Hero.Content>
     </Hero>
   )
 }
 
+export const HeroWithFigure: Story<HeroProps> = (args) => {
+  return (
+    <Hero {...args}>
+      <Hero.Content>
+        <img
+          src="https://api.lorem.space/image/movie?w=260&h=400"
+          className="max-w-sm rounded-lg shadow-2xl"
+        />
+        <div>
+          <h1 className="text-5xl font-bold">Box Office News!</h1>
+          <p className="py-6">
+            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
+            a id nisi.
+          </p>
+          <Button color="primary">Get Started</Button>
+        </div>
+      </Hero.Content>
+    </Hero>
+  )
+}
+
+export const HeroWithForm: Story<HeroProps> = (args) => {
+  return (
+    <Hero {...args}>
+      <Hero.Content className="flex-col lg:flex-row-reverse">
+        <div className="text-center lg:text-left">
+          <h1 className="text-5xl font-bold">Login now!</h1>
+          <p className="py-6">
+            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
+            a id nisi.
+          </p>
+        </div>
+        <Card className="flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+          <Card.Body>
+            <Form>
+              <Form.Label title="Email" />
+              <Input
+                type="text"
+                placeholder="email"
+                className="input-bordered"
+              />
+            </Form>
+            <Form>
+              <Form.Label title="Password" />
+              <Input
+                type="text"
+                placeholder="password"
+                className="input-bordered"
+              />
+              <label className="label">
+                <Link href="#" className="label-text-alt" hover>
+                  Forgot password?
+                </Link>
+              </label>
+            </Form>
+            <Form className="mt-6">
+              <Button>Login</Button>
+            </Form>
+          </Card.Body>
+        </Card>
+      </Hero.Content>
+    </Hero>
+  )
+}
+
+export const HeroWithOverlayImage: Story<HeroProps> = (args) => {
+  return (
+    <Hero
+      style={{
+        backgroundImage:
+          'url(https://api.lorem.space/image/fashion?w=1000&h=800)',
+      }}
+    >
+      <Hero.Overlay {...args} />
+      <Hero.Content className="text-center">
+        <div className="max-w-md">
+          <h1 className="text-5xl font-bold">Hello there</h1>
+          <p className="py-6">
+            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
+            a id nisi.
+          </p>
+
+          <Button color="primary">Get Started</Button>
+        </div>
+      </Hero.Content>
+    </Hero>
+  )
+}
+
+HeroWithOverlayImage.args = { className: 'bg-opacity-60' }
 Default.args = {}
