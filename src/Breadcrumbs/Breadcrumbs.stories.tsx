7b97f481f087c97349f6c934e9e51c545ee560b6
yoshi6jp <yoshi6jp@gmail.com>
2022-06-24 23:10:40 +0900

    docs: added storybook for breadcrumbs, menu, and tabs (#141)

--- src/Breadcrumbs/Breadcrumbs.stories.tsx
+++ src/Breadcrumbs/Breadcrumbs.stories.tsx
@@ -19,4 +19,74 @@ export const Default: Story<BreadcrumbsProps> = (args) => {
     </Breadcrumbs>
   )
 }
-Default.args = {}
+
+export const WithIcons: Story<BreadcrumbsProps> = (args) => {
+  return (
+    <Breadcrumbs {...args}>
+      <Breadcrumbs.Item href="/">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          className="w-4 h-4 mr-2 stroke-current"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            d="M3 7v10a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-6l-2-2H5a2 2 0 00-2 2z"
+          />
+        </svg>
+        Home
+      </Breadcrumbs.Item>
+      <Breadcrumbs.Item href="/">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          className="w-4 h-4 mr-2 stroke-current"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            d="M3 7v10a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-6l-2-2H5a2 2 0 00-2 2z"
+          />
+        </svg>
+        Documents
+      </Breadcrumbs.Item>
+      <Breadcrumbs.Item href="/">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          className="w-4 h-4 mr-2 stroke-current"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            d="M9 13h6m-3-3v6m5 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"
+          />
+        </svg>
+        Add Document
+      </Breadcrumbs.Item>
+    </Breadcrumbs>
+  )
+}
+
+export const WithMaxWidth: Story<BreadcrumbsProps> = (args) => {
+  return (
+    <Breadcrumbs {...args}>
+      <Breadcrumbs.Item>Long text 1</Breadcrumbs.Item>
+      <Breadcrumbs.Item>Long text 2</Breadcrumbs.Item>
+      <Breadcrumbs.Item>Long text 3</Breadcrumbs.Item>
+      <Breadcrumbs.Item>Long text 4</Breadcrumbs.Item>
+      <Breadcrumbs.Item>Long text 5</Breadcrumbs.Item>
+    </Breadcrumbs>
+  )
+}
+
+WithMaxWidth.args = {
+  className: 'max-w-xs ',
+}
--- src/Tabs/Tabs.stories.tsx
+++ src/Tabs/Tabs.stories.tsx
@@ -11,6 +11,9 @@ export default {
   parameters: {
     controls: { exclude: ['ref'] },
   },
+  args: {
+    value: 1,
+  },
 } as Meta
 
 export const Default: Story<TabsProps<number>> = (args) => {
@@ -23,4 +26,29 @@ export const Default: Story<TabsProps<number>> = (args) => {
   )
 }
 
-Default.args = {}
+export const Sizes: Story<TabsProps<number>> = ({ size, ...args }) => {
+  return (
+    <div className="flex flex-col gap-4 items-center">
+      <Tabs {...args} size="xs">
+        <Tab value={0}>Tiny</Tab>
+        <Tab value={1}>Tiny</Tab>
+        <Tab value={2}>Tiny</Tab>
+      </Tabs>
+      <Tabs {...args} size="sm">
+        <Tab value={0}>Small</Tab>
+        <Tab value={1}>Small</Tab>
+        <Tab value={2}>Small</Tab>
+      </Tabs>
+      <Tabs {...args} size="md">
+        <Tab value={0}>Normal</Tab>
+        <Tab value={1}>Normal</Tab>
+        <Tab value={2}>Normal</Tab>
+      </Tabs>
+      <Tabs {...args} size="lg">
+        <Tab value={0}>Large</Tab>
+        <Tab value={1}>Large</Tab>
+        <Tab value={2}>Large</Tab>
+      </Tabs>
+    </div>
+  )
+}
