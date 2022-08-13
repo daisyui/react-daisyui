5e5dbc7e77686f66864982e1deabaa8b9f69e71d
Yoshiharu KAMATA <yoshi6jp@gmail.com>
2022-08-13 21:33:41 +0900

    add disabled buttons story

--- src/BottomNavigation/BottomNavigation.stories.tsx
+++ src/BottomNavigation/BottomNavigation.stories.tsx
@@ -220,3 +220,50 @@ export const Sizes: Story<BottomNavigationProps> = (args) => {
     </div>
   )
 }
+
+export const DisabledButtons: Story<BottomNavigationProps> = (args) => {
+  return (
+    <div className="preview bg-base-200 w-full">
+      <BottomNavigation {...args} className="relative">
+        <Home />
+        <button disabled>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-5 w-5"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+            />
+          </svg>
+        </button>
+        <Statics />
+      </BottomNavigation>
+      <BottomNavigation {...args} className="relative">
+        <Home />
+        <button className="disabled" role="button" aria-describedby="true">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-5 w-5"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+            />
+          </svg>
+        </button>
+        <Statics />
+      </BottomNavigation>
+    </div>
+  )
+}
