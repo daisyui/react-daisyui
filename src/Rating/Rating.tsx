2708419c0c8f9d3ac8e630904576db94799ee39d
Klaas058 <Klaas.vanderwerk@gmail.com>
2022-10-10 17:56:05 +0200

    fix(Rating): rebuild component to a controlled React component (#222)
    * fix collapse checkbox not closing on ios while maintaining focusable tabindex on the component + bring docs inline with daisyui
    
    * Refactor the Rating component into an actual controlled component
    
    * Remove leftover debug
    
    * PR feedback - refactor rating to value and onRatingChange to onChange

--- src/Rating/Rating.tsx
+++ src/Rating/Rating.tsx
@@ -1,32 +1,47 @@
-import React from 'react'
+import React, {ReactElement, useEffect, useState} from 'react'
 import clsx from 'clsx'
 import { twMerge } from 'tailwind-merge'
 
 import { IComponentBaseProps, ComponentSize } from '../types'
 
-import RatingItem from './RatingItem'
+import RatingItem, { RatingItemProps } from './RatingItem'
 
 export type RatingProps = React.HTMLAttributes<HTMLDivElement> &
   IComponentBaseProps & {
     size?: ComponentSize
     half?: boolean
     hidden?: boolean
+    value: number
+    onChange?: (newRating: number) => void
   }
 
 const Rating = React.forwardRef<HTMLDivElement, RatingProps>(
   (
-    { children, size, half, hidden, dataTheme, className, ...props },
+    { children, size, half, hidden, dataTheme, className, value, onChange, ...props },
     ref
   ): JSX.Element => {
+    const [rating, setRating] = useState(value)
     const classes = twMerge(
       'rating',
       className,
       clsx({
         [`rating-${size}`]: size,
         'rating-half': half,
-        'rating-hidden': hidden,
+        'rating-hidden': hidden || rating === 0,
       })
     )
+
+    const handleOnRatingChange = (newRating: number) => {
+      if (newRating === rating) {
+        return
+      }
+      setRating(newRating)
+      onChange?.(newRating)
+    }
+
+    useEffect(() => {
+      setRating(value)
+    }, [value])
 
     return (
       <div
@@ -36,7 +51,22 @@ const Rating = React.forwardRef<HTMLDivElement, RatingProps>(
         data-theme={dataTheme}
         className={classes}
       >
-        {children}
+        {rating === 0 && (
+          <RatingItem
+            className={clsx(classes, 'hidden')}
+            checked
+            readOnly
+          />
+        )}
+        {React.Children.map(children, (child, index) => {
+          const childComponent = child as ReactElement<RatingItemProps>
+          return React.cloneElement(childComponent, {
+            key: index + rating,
+            checked: rating === index + 1,
+            readOnly: onChange == null,
+            onChange: () => handleOnRatingChange(index + 1),
+          })
+        })}
       </div>
     )
   }
--- src/Rating/RatingItem.tsx
+++ src/Rating/RatingItem.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export type RatingItemProps = React.InputHTMLAttributes<HTMLInputElement>
 
 const RatingItem = ({ ...props }: RatingItemProps): JSX.Element => {
-  return <input {...props} type="radio" />
+  return <input {...props} type="checkbox" />
 }
 
 export default RatingItem
