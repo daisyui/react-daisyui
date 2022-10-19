220a289e23777ac411413483bd06fcc419d183c9
Henrique Gouvêa Bonfim <94474960+Henrique-Gouvea@users.noreply.github.com>
2022-10-19 09:57:02 -0300

    test: Footer (#237)
    * feat: Starting test footer
    
    * feat(test-footer): should render footer title
    
    * feat: footer title have className
    
    * feat: Footer links have className
    
    * feat: should render footer link list

---
+++ src/Footer/Footer.test.tsx
@@ -0,0 +1,65 @@
+import '@testing-library/jest-dom'
+import '@testing-library/jest-dom/extend-expect'
+
+import { render, screen } from '@testing-library/react'
+
+import Footer from '.'
+
+describe('Footer', () => {
+  const footerTitle = 'Services'
+  const linkList = ['Branding', 'Design', 'Marketing', 'Advertisement']
+
+  it('Should render Footer title', () => {
+    render(
+      <Footer className="p-10 bg-neutral text-neutral-content">
+      <div>
+        <Footer.Title>{footerTitle}</Footer.Title>
+      </div>
+    </Footer>
+    )
+    expect(screen.getByText(footerTitle)).toBeInTheDocument()
+  })
+
+  it('Footer title have className "p-10 bg-neutral text-neutral-content"', () => {
+    const {container} = render(
+      <Footer className="p-10 bg-neutral text-neutral-content">
+      <div>
+        <Footer.Title>{footerTitle}</Footer.Title>
+      </div>
+    </Footer>
+    )
+    const titleClassName = container.getElementsByClassName('p-10 bg-neutral text-neutral-content')
+    expect(titleClassName).toBeTruthy()
+  })
+
+  it('Footer links have className "link link-hover"', () => {
+    const {container} = render(
+      <Footer className="p-10 bg-neutral text-neutral-content">
+      <div>
+        <Footer.Title>{footerTitle}</Footer.Title>
+        {linkList.map((links) => (
+          <a key= {links} className="link link-hover">{links}</a>
+        ))}
+      </div>
+    </Footer>
+    )
+    expect(screen.getByText(linkList[0])).toBeInTheDocument()
+    const quantityLinks = container.getElementsByClassName('link link-hover')
+    expect(quantityLinks.length).toBe(4)
+  })
+
+  it('Should render Footer link list', () => {
+    const {container} = render(
+      <Footer className="p-10 bg-neutral text-neutral-content">
+      <div>
+        <Footer.Title>{footerTitle}</Footer.Title>
+        {linkList.map((links) => (
+          <a key= {links} className="link link-hover">{links}</a>
+        ))}
+      </div>
+    </Footer>
+    )
+    const linksClassName = container.getElementsByClassName('link link-hover')
+    expect(linksClassName).toBeTruthy()
+  })
+})
