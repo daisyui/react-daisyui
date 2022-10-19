fa1a4afe14bcef6cea44963b3f77a18163c7e924
Henrique-Gouvea <henrque.bonfim2@gmail.com>
2022-10-19 07:56:53 -0300

    feat(test-footer): should render footer title

--- src/Footer/Footer.test.tsx
+++ src/Footer/Footer.test.tsx
@@ -6,5 +6,16 @@ import { render, screen } from '@testing-library/react'
 import Footer from '.'
 
 describe('Footer', () => {
+  const footerTitle = 'Services'
   
+  it('Should render Footer title', () => {
+    render(
+      <Footer className="p-10 bg-neutral text-neutral-content">
+      <div>
+        <Footer.Title>{footerTitle}</Footer.Title>
+      </div>
+    </Footer>
+    )
+    expect(screen.getByText(footerTitle)).toBeInTheDocument()
+  })
 })
