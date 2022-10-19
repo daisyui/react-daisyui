f66eaced5590a98c25a0f34b37f089e5522383f6
Henrique-Gouvea <henrque.bonfim2@gmail.com>
2022-10-19 08:03:23 -0300

    feat: Footer links have className

--- src/Footer/Footer.test.tsx
+++ src/Footer/Footer.test.tsx
@@ -7,6 +7,7 @@ import Footer from '.'
 
 describe('Footer', () => {
   const footerTitle = 'Services'
+  const linkList = ['Branding', 'Design', 'Marketing', 'Advertisement']
 
   it('Should render Footer title', () => {
     render(
@@ -29,5 +30,21 @@ describe('Footer', () => {
     )
     const titleClassName = container.getElementsByClassName('p-10 bg-neutral text-neutral-content')
     expect(titleClassName).toBeTruthy()
+  })
+
+  it('Footer links have className "link link-hover"', () => {
+    const {container} = render(
+      <Footer className="p-10 bg-neutral text-neutral-content">
+      <div>
+        <Footer.Title>{footerTitle}</Footer.Title>
+        {linkList.map((links) => (
+          <a key= {links} className="link link-hover">{links}</a>
+        ))}
+      </div>
+    </Footer>
+    )
+    expect(screen.getByText(linkList[0])).toBeInTheDocument()
+    const quantityLinks = container.getElementsByClassName('link link-hover')
+    expect(quantityLinks.length).toBe(4)
   })
 })
