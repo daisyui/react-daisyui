cc44109245ec3e7b6d4b8b0a97dd4d6f9cc3c861
Henrique-Gouvea <henrque.bonfim2@gmail.com>
2022-10-19 08:00:49 -0300

    feat: footer title have className

--- src/Footer/Footer.test.tsx
+++ src/Footer/Footer.test.tsx
@@ -7,7 +7,7 @@ import Footer from '.'
 
 describe('Footer', () => {
   const footerTitle = 'Services'
-  
+
   it('Should render Footer title', () => {
     render(
       <Footer className="p-10 bg-neutral text-neutral-content">
@@ -17,5 +17,17 @@ describe('Footer', () => {
     </Footer>
     )
     expect(screen.getByText(footerTitle)).toBeInTheDocument()
+  })
+
+  it('Footer title have className "p-10 bg-neutral text-neutral-content"', () => {
+    const {container} = render(
+      <Footer className="p-10 bg-neutral text-neutral-content">
+      <div>
+        <Footer.Title>{footerTitle}</Footer.Title>
+      </div>
+    </Footer>
+    )
+    const titleClassName = container.getElementsByClassName('p-10 bg-neutral text-neutral-content')
+    expect(titleClassName).toBeTruthy()
   })
 })
