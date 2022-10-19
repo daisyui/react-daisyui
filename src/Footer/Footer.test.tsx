41904501a64deadf185e4a375265a4aa606be4cc
Henrique-Gouvea <henrque.bonfim2@gmail.com>
2022-10-19 07:55:20 -0300

    feat: Starting test footer

---
+++ src/Footer/Footer.test.tsx
@@ -0,0 +1,10 @@
+import '@testing-library/jest-dom'
+import '@testing-library/jest-dom/extend-expect'
+
+import { render, screen } from '@testing-library/react'
+
+import Footer from '.'
+
+describe('Footer', () => {
+  
+})
