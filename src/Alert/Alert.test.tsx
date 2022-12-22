f289fc5b4025579bc50d3c65e55b6eed22c1c8e3
Yuri <118747540+sldk-yuri@users.noreply.github.com>
2022-12-22 03:11:20 +0300

    test(alert): adds test cases for the Alert (#277)

---
+++ src/Alert/Alert.test.tsx
@@ -0,0 +1,33 @@
+import { render, screen, within } from '@testing-library/react'
+import { createRef } from 'react'
+import Alert from './Alert'
+
+describe('Alert', () => {
+  it('renders a default state', () => {
+    render(<Alert>Test</Alert>)
+    expect(screen.getByRole('alert')).toBeTruthy()
+  })
+
+  it('renders an icon', () => {
+    render(<Alert icon={<div data-testid="foo" />}>Test</Alert>)
+    expect(screen.getByTestId('foo')).toBeTruthy()
+  })
+
+  it('renders a status', () => {
+    render(<Alert status="success">Test</Alert>)
+    expect(screen.getByRole('alert')).toHaveClass('alert-success')
+  })
+
+  it('renders an innerClassName', () => {
+    render(<Alert innerClassName="font-bold">Test</Alert>)
+    expect(within(screen.getByRole('alert')).getByText(/test/i)).toHaveClass(
+      'font-bold'
+    )
+  })
+
+  it('should be able to access the native div', () => {
+    const alertRef = createRef<HTMLDivElement>()
+    render(<Alert ref={alertRef} />)
+    expect(screen.getByRole('alert')).toEqual(alertRef.current)
+  })
+})
