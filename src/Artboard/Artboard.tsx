a4dca706959e92273bb3d15a30eb3888ac2ce81d
Benjamin Trosch <benjitrosch@gmail.com>
2023-04-03 15:16:12 -0400

    fix(Artboard): hardcoded sizes

--- src/Artboard/Artboard.tsx
+++ src/Artboard/Artboard.tsx
@@ -6,21 +6,27 @@ import { IComponentBaseProps } from '../types'
 
 export type ArtboardProps = React.HTMLAttributes<HTMLDivElement> &
   IComponentBaseProps & {
+    demo?: boolean
     size?: 1 | 2 | 3 | 4 | 5 | 6
     horizontal?: boolean
   }
 
 const Artboard = forwardRef<HTMLDivElement, ArtboardProps>(
   (
-    { children, size, horizontal, dataTheme, className, ...props },
+    { children, demo, size, horizontal, dataTheme, className, ...props },
     ref
   ): JSX.Element => {
     const classes = twMerge(
       'artboard',
-      'artboard-demo',
       className,
       clsx({
-        [`phone-${size}`]: size,
+        'artboard-demo': demo,
+        'phone-1': size === 1,
+        'phone-2': size === 2,
+        'phone-3': size === 3,
+        'phone-4': size === 4,
+        'phone-5': size === 5,
+        'phone-6': size === 6,
         horizontal: horizontal,
       })
     )
