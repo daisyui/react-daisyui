6d0da0f92d89d48e0555010436f89e5b327928d5
Yoshiharu KAMATA <yoshi6jp@gmail.com>
2023-06-10 07:34:03 +0900

    using dialog element

--- src/Modal/index.tsx
+++ src/Modal/index.tsx
@@ -1,3 +1,9 @@
-import Modal, { ModalProps as TModalProps } from './Modal'
+import Modal, {
+  ModalProps as TModalProps,
+  DialogProps as TDialogProps,
+} from './Modal'
+import { ModalProps as TModalLegacyProps } from './ModalLegacy'
 export type ModalProps = TModalProps
+export type ModalLegacyProps = TModalLegacyProps
+export type DialogProps = TDialogProps
 export default Modal
