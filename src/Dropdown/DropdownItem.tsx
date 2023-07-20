71714a169c2602d3bc579c22fd8fd1d80e8ff7ca
Lawrence Onah <paplow01@gmail.com>
2023-07-20 16:25:04 +0100

    fix(DropdownItem): improved prop type check

--- src/Dropdown/DropdownItem.tsx
+++ src/Dropdown/DropdownItem.tsx
@@ -1,7 +1,12 @@
 import React from 'react'
 
-export type DropdownItemProps =
-  React.AnchorHTMLAttributes<HTMLAnchorElement> & { anchor?: boolean }
+type Anchor = React.AnchorHTMLAttributes<HTMLAnchorElement> & {
+  anchor?: true
+}
+
+type NoAnchor = Pick<Anchor, 'children'> & { anchor?: false }
+
+export type DropdownItemProps = Anchor | NoAnchor
 
 const DropdownItem = React.forwardRef<HTMLAnchorElement, DropdownItemProps>(
   ({ anchor = true, ...props }, ref) => {
