57763d7c3b84f245e1de6c72dca2217d40bc2be2
benjitrosch <benjitrosch@users.noreply.github.com>
2023-07-28 00:35:29 +0000

    docs: update storybook for latest release

--- docs/assets/Indicator.stories-9d07f3b0.js
+++
@@ -1,4 +0,0 @@
-import{j as o}from"./tw-merge-e2243cb0.js";import{I as s}from"./index-15b94d0e.js";import{A as i}from"./index-c5432677.js";import{B as c}from"./index-a68f1c8f.js";import"./index-ebeaab24.js";import"./clsx.m-1229b3e0.js";import"./utils-9cddd062.js";const I={title:"Layout/Indicator",component:s,argTypes:{innerRef:{control:!1},children:{control:!1},item:{control:!1}}},r=n=>o(s,{...n});r.args={children:o(i,{src:"http://daisyui.com/tailwind-css-component-profile-1@94w.png"}),item:o(c,{color:"error"})};var t,e,a;r.parameters={...r.parameters,docs:{...(t=r.parameters)==null?void 0:t.docs,source:{originalSource:`args => {
-  return <Indicator {...args} />;
-}`,...(a=(e=r.parameters)==null?void 0:e.docs)==null?void 0:a.source}}};const h=["Default"];export{r as Default,h as __namedExportsOrder,I as default};
-//# sourceMappingURL=Indicator.stories-9d07f3b0.js.map
---
+++ docs/assets/preview-ffb96a9b.js
@@ -0,0 +1,2 @@
+import{_ as e}from"./iframe-5d151793.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-3PUGWF3O-50357613.js"),["./DocsRenderer-3PUGWF3O-50357613.js","./iframe-5d151793.js","./index-ebeaab24.js","./react-18-8ac2d3a8.js","./index-073301bc.js","./index-59fd8170.js","./chunk-H6ZRF7XV-146ce0bc.js","./index-d475d2ea.js","./index-356e4a49.js"],import.meta.url);return new r}}};export{a as parameters};
+//# sourceMappingURL=preview-ffb96a9b.js.map
