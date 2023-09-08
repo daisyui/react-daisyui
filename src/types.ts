4f01cbe1bc2a1b9371cf627bd3688683bdf214bd
Yoshiharu KAMATA <yoshi6jp@gmail.com>
2023-09-08 17:46:51 +0900

    fix: Revert update by prettier.

--- src/types.ts
+++ src/types.ts
@@ -9,19 +9,19 @@ import {
 } from './constants'
 import { DEFAULT_THEMES } from './defaultThemes'
 
-export type DataTheme = (typeof DEFAULT_THEMES)[number] | string
+export type DataTheme = typeof DEFAULT_THEMES[number] | string
 
 export interface IComponentBaseProps {
   dataTheme?: DataTheme
 }
 
-export type ComponentColor = (typeof componentColors)[number]
+export type ComponentColor = typeof componentColors[number]
 
-export type ComponentPosition = (typeof componentPositions)[number]
-export type ComponentShape = (typeof componentShapes)[number]
-export type ComponentSize = (typeof componentSizes)[number]
-export type ComponentStatus = (typeof componentStatuses)[number]
-export type ComponentBrandColors = (typeof brandColors)[number]
-export type ComponentBgColors = (typeof bgColors)[number]
+export type ComponentPosition = typeof componentPositions[number]
+export type ComponentShape = typeof componentShapes[number]
+export type ComponentSize = typeof componentSizes[number]
+export type ComponentStatus = typeof componentStatuses[number]
+export type ComponentBrandColors = typeof brandColors[number]
+export type ComponentBgColors = typeof bgColors[number]
 
 export type ListOrItem<T> = T[] | T | Array<T | T[]>
