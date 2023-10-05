9faae639f0b989466658ddde97f3ec148f00f644
benjitrosch <benjitrosch@users.noreply.github.com>
2023-10-05 20:52:35 +0000

    docs: update storybook for latest release

--- docs/assets/syntaxhighlighter-2MZ4Y3HO-886d9f39.js
+++
@@ -1,2 +0,0 @@
-import{S as c,c as l,s as n}from"./index-ad7f205d.js";import"./iframe-9b9cee73.js";import"../sb-preview/runtime.js";import"./chunk-H6ZRF7XV-146ce0bc.js";import"./index-d475d2ea.js";import"./index-ebeaab24.js";import"./index-073301bc.js";import"./index-356e4a49.js";export{c as SyntaxHighlighter,l as createCopyToClipboardFunction,n as default};
-//# sourceMappingURL=syntaxhighlighter-2MZ4Y3HO-886d9f39.js.map
