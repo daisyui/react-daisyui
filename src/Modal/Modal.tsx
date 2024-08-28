4bce97fda4ba03d5897690948ec47edf0ad6fb27
yosipy <todacchi.desu@gmail.com>
2024-08-29 01:46:44 +0900

    refactor: remove ModalLegacyProps as it is not used

--- src/Modal/Modal.tsx
+++ src/Modal/Modal.tsx
@@ -7,7 +7,7 @@ import { IComponentBaseProps } from '../types'
 import ModalActions from './ModalActions'
 import ModalBody from './ModalBody'
 import ModalHeader from './ModalHeader'
-import ModalLegacy, { ModalProps as ModalLegacyProps } from './ModalLegacy'
+import ModalLegacy from './ModalLegacy'
 
 export type ModalProps = React.DialogHTMLAttributes<HTMLDialogElement> &
   IComponentBaseProps & {
