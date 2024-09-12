d380726ae292530ae9a2a846513b13f4a0b8b0ed
Andrew Brough <andrew@kahi.io>
2024-09-12 13:01:13 -0400

    Added noCell property to Table.Head, Body, Footer to allow no wrapping children, allowing customization of pinned headers everywhere without getting rid of existing wrapping functionality.

--- src/Table/TableFooter.tsx
+++ src/Table/TableFooter.tsx
@@ -6,17 +6,20 @@ export type TableFooterProps =
   React.TableHTMLAttributes<HTMLTableSectionElement> &
     IComponentBaseProps & {
       children?: ReactElement[]
+      noCell?: boolean // don't wrap children in th/td, should pass children as th/tds manually
     }
 
 const TableFooter = React.forwardRef<HTMLTableSectionElement, TableFooterProps>(
-  ({ children, ...props }, ref): JSX.Element => {
+  ({ children, noCell = false, ...props }, ref): JSX.Element => {
+    const renderChildren = noCell
+      ? children
+      : children?.map((child, i) =>
+          i < 1 ? <th key={i}>{child}</th> : <td key={i}>{child}</td>
+        )
+
     return (
       <tfoot {...props} ref={ref}>
-        <tr>
-          {children?.map((child, i) => {
-            return <th key={i}>{child}</th>
-          })}
-        </tr>
+        <tr>{renderChildren}</tr>
       </tfoot>
     )
   }
--- src/Table/TableRow.tsx
+++ src/Table/TableRow.tsx
@@ -1,5 +1,5 @@
-import React, { ReactElement } from 'react'
 import clsx from 'clsx'
+import React, { ReactElement } from 'react'
 import { twMerge } from 'tailwind-merge'
 
 import { IComponentBaseProps } from '../types'
@@ -9,10 +9,14 @@ export type TableRowProps = React.TableHTMLAttributes<HTMLTableRowElement> &
     children?: ReactElement[]
     active?: boolean
     hover?: boolean
+    noCell?: boolean // don't wrap children in th/td, should pass children as th/tds manually
   }
 
 const TableRow = React.forwardRef<HTMLTableRowElement, TableRowProps>(
-  ({ children, active, hover, className, ...props }, ref): JSX.Element => {
+  (
+    { children, active, hover, noCell = false, className, ...props },
+    ref
+  ): JSX.Element => {
     const classes = twMerge(
       className,
       clsx({
@@ -21,11 +25,15 @@ const TableRow = React.forwardRef<HTMLTableRowElement, TableRowProps>(
       })
     )
 
+    const renderChildren = noCell
+      ? children
+      : children?.map((child, i) =>
+          i < 1 ? <th key={i}>{child}</th> : <td key={i}>{child}</td>
+        )
+
     return (
       <tr {...props} className={classes} ref={ref}>
-        {children?.map((child, i) => 
-          i < 1 ? <th key={i}>{child}</th> : <td key={i}>{child}</td>
-        )}
+        {renderChildren}
       </tr>
     )
   }
