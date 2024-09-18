fa116f58fcabe8ff7efc484042df45868a15ba07
benjitrosch <benjitrosch@users.noreply.github.com>
2024-09-18 18:58:35 +0000

    docs: update storybook for latest release

---
+++ docs/assets/syntaxhighlighter-MJWPISIS-3c92a47e.js
@@ -0,0 +1,1 @@
+import{S as c,c as l,s as n}from"./index-41eec189.js";import"./iframe-80500056.js";import"../sb-preview/runtime.js";import"./chunk-6E673XPF-4294b5bd.js";import"./index-f46741a2.js";import"./index-d868a670.js";import"./index-b7b0ffbb.js";import"./index-356e4a49.js";export{c as SyntaxHighlighter,l as createCopyToClipboardFunction,n as default};
