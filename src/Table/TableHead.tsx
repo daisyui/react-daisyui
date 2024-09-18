0317ed36ab92c717cecf738f18e71154a49dd2b7
Andrew Brough <spectrum1012@gmail.com>
2024-09-18 12:43:29 -0400

    chore(Table): more flexible Table.Head/Footer with [noCell] prop for better pinCols support (#463)
    * Added noCell property to Table.Head, Body, Footer to allow no wrapping children, allowing customization of pinned headers everywhere without getting rid of existing wrapping functionality.
    
    * added Table.Head, Row, Footer children/th/td existence tests
    
    * Added new pinned rows and cols story with noCell example.
    
    * better story with more data to better demonstrate pinned headers which aren't the first child.

--- src/Table/TableHead.tsx
+++ src/Table/TableHead.tsx
@@ -3,17 +3,20 @@ import React, { ReactNode } from 'react'
 export type TableHeadProps =
   React.TableHTMLAttributes<HTMLTableSectionElement> & {
     children?: ReactNode[]
+    noCell?: boolean // don't wrap children in th/td, should pass children as th/tds manually
   }
 
 const TableHead = React.forwardRef<HTMLTableSectionElement, TableHeadProps>(
-  ({ children, ...props }, ref): JSX.Element => {
+  ({ children, noCell = false, ...props }, ref): JSX.Element => {
+    const renderChildren = noCell
+      ? children
+      : children?.map((child, i) =>
+          i < 1 ? <th key={i}>{child}</th> : <td key={i}>{child}</td>
+        )
+
     return (
       <thead {...props} ref={ref}>
-        <tr>
-          {children?.map((child, i) => {
-            return <th key={i}>{child}</th>
-          })}
-        </tr>
+        <tr>{renderChildren}</tr>
       </thead>
     )
   }
