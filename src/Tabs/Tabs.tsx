ef6a5207ebcb4ef9b8bbaf4b32508fcffe4a9f98
Benjamin Trosch <benjitrosch@gmail.com>
2025-04-16 16:21:04 -0400

    chore(Tabs): update to 5.0.0

--- src/Tabs/Tabs.tsx
+++ src/Tabs/Tabs.tsx
@@ -2,30 +2,34 @@ import React, { forwardRef } from 'react'
 import clsx from 'clsx'
 import { twMerge } from 'tailwind-merge'
 
-import { IComponentBaseProps, ComponentSize } from '../types'
+import { IComponentBaseProps, ComponentSize, ComponentPosition } from '../types'
 
 import Tab from './Tab'
 import RadioTab from './RadioTab'
 
 export type TabsProps = React.HTMLAttributes<HTMLDivElement> &
   IComponentBaseProps & {
-    variant?: 'bordered' | 'lifted' | 'boxed'
+    variant?: 'bordered' | 'lift' | 'boxed'
     size?: ComponentSize
+    position?: Extract<ComponentPosition, 'top' | 'bottom'>
   }
 
 const Tabs = forwardRef<HTMLDivElement, TabsProps>(
-  ({ children, className, variant, size }, ref): JSX.Element => {
+  ({ children, className, variant, size, position }, ref): JSX.Element => {
     const classes = twMerge(
       'tabs',
       className,
       clsx({
         'tabs-boxed': variant === 'boxed',
         'tabs-bordered': variant === 'bordered',
-        'tabs-lifted': variant === 'lifted',
+        'tabs-lift': variant === 'lift',
+        'tabs-xl': size === 'xl',
         'tabs-lg': size === 'lg',
         'tabs-md': size === 'md',
         'tabs-sm': size === 'sm',
         'tabs-xs': size === 'xs',
+        'tabs-top': position === 'top',
+        'tabs-bottom': position === 'bottom',
       })
     )
 
